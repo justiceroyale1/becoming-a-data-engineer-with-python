8225c938612a33ba1c387f214ed0c9f7d477e381
Justice Abutu <justiceroyale1@gmail.com>
2025-08-24 19:14:15 +0100

    Add code sample for Tuples and Sequences

---
+++ tuples-sequences/example-one.py
@@ -0,0 +1,16 @@
+t = 12345, 54321, 'hello!'
+print(t[0])
+
+print(t)
+
+u = t, (1, 2, 3, 4, 5)
+print(u)
+
+# t[0] = 88888
+
+v = ([1, 2, 3], [3, 2, 1])
+print(v)
+
+v[0].append(32)
+
+print(v)
---
+++ tuples-sequences/example-three.py
@@ -0,0 +1,8 @@
+t = 12345, 54321, 'hello!'
+
+x, y, z = t
+
+print('t=', t)
+print("x=", x)
+print('y=', y)
+print('z=', z)
