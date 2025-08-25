39ebd940c2548a69b63546623a3eee1db08b0afd
Justice Abutu <justiceroyale1@gmail.com>
2025-08-25 07:55:06 +0100

    Add code samples for sets

---
+++ sets/example-one.py
@@ -0,0 +1,17 @@
+basket = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana'}
+# print(basket)
+
+# print('orange' in basket)
+
+# print('crabgrass' in basket)
+
+
+a = set('abracadabra')
+b = set('alacazam')
+
+print('a=', a)
+print('b=', b)
+print('a-b=', a-b)
+print('a|b=', a | b)
+print('a&b=', a & b)
+print('a ^ b=', a ^ b)
